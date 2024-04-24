96eb2408c91d9c3ea9dd6a31ef3f655210be5cbd
dayvidwhy <dayvidwhy@gmail.com>
2024-04-24 13:39:26 +1000

    Update readme for development. /views endpoint can read JSON body params.

--- README.md
+++ README.md
@@ -23,6 +23,18 @@ Build and run the server.
 ```bash
 go build -o bin/server server.go
 ./bin/server
+
+# or with go run
+go run server.go
 ```
 
 Server will be available at `localhost:1323` on your machine.
+
+## Development
+Attach VSCode to the running `insights-app` container and install the Go extension for autocompletion. This saves needing to install Go locally and speeds up the setup.
+
+`>Dev Containers: Attach to Running Container...`
+
+Select `/insights-app`.
+
+VSCode will recommend the Go extension when you open a `.go` file.
--- server.go
+++ server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 type Response struct {
 	Status string `json:"status"`
+	Url    string `json:"url"`
+}
 
+type PageView struct {
+	Url string `json:"url"`
 }
 
 func main() {
@@ -16,10 +22,25 @@ func main() {
 		return c.String(http.StatusOK, "Blank")
 	})
 
-	e.POST("/view", func(c echo.Context) error {
-		response := &Response{
-			Status: "Saved",
+	e.POST("/views", func(c echo.Context) error {
+		// get the JSON body from the request
+		u := new(PageView)
+		if err := c.Bind(u); err != nil {
+			return err
 		}
+
+		fmt.Println("Tracking URL: " + u.Url)
+
+		// build response
+		response := map[string]string{
+			"status": "success",
+			"url":    u.Url,
+		}
+
+		if u.Url == "" {
+			response["status"] = "Empty"
+		}
+
 		return c.JSON(http.StatusOK, response)
 	})
 
